0747ac7e149a3e2ae43a8ffc82f7ea98df3145a9
type89 <zong1yuan2@gmail.com>
2020-09-06 19:24:17 +0900

    First commit

---
+++ nyumon_biz.py
@@ -0,0 +1,73 @@
+from __future__ import print_function
+from googleapiclient.discovery import build
+from googleapiclient import discovery
+from apiclient.http import MediaFileUpload
+from httplib2 import Http
+from oauth2client import file, client, tools
+import json
+import base64
+from email.mime.text import MIMEText
+from email.utils import formatdate
+#import pandas as pd
+import datetime
+import subprocess
+import glob
+import os
+
+# Setup the Drive v3 API
+SECRET_KEY = 'test_pg.json'
+SCOPE1 = 'https://www.googleapis.com/auth/drive'
+SCOPE2 = 'https://www.googleapis.com/auth/spreadsheets'
+SCOPE3 = 'https://mail.google.com/'
+SCOPE4 = 'https://www.googleapis.com/auth/calendar'
+SCOPES = SCOPE1 + ' ' + SCOPE2 + ' ' + SCOPE3 + ' ' + SCOPE4
+GDRIVE_FOLDER_ID = '1KM0BjZNDgxID8ZP97opIiKVyHqsA5XaN'
+REC_FOLDER = "/home/pi/nhk"
+CHANNNEL = "NHK2"
+MAIL_FROM = "zong1yuan2@gmail.com"
+MAIL_TO = "zong1yuan2@gmail.com, zong1yuan2@live.jp"
+
+
+def get_credential():
+    store = file.Storage('/home/pi/credentials.json')
+    creds = store.get()
+
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets(SECRET_KEY, SCOPES)
+        creds = tools.run_flow(flow, store)
+
+    return creds
+
+def rec_radio(channel, rectime, rec_folder):
+    cmd = 'sh /home/pi/rec_nhk.sh' + " " + channel + " " + str(rectime) + " " + rec_folder
+    process = (subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True).communicate()[0]).decode('utf-8')
+    #print('コマンドは\n'+process+'です')#何かしらの処理
+
+def upload_data(creds, rec_folder, upload_folder):
+    service = build('drive', 'v3', http=creds.authorize(Http()))
+    os.chdir(rec_folder)
+    files = glob.glob("./*")
+    for file in files:
+        print("FILE ===> " + file)
+        filename = file.replace('./NHK2_', '入門ビジネス英語_')
+
+        file_metadata = {
+            'name': filename,
+            'mimeType': "audio/mpeg",
+            'parents': [upload_folder]
+            }
+        media = MediaFileUpload(str(file),
+                            mimetype='application/vnd.google-apps-document',
+                               resumable=True)
+        service.files().create(body=file_metadata,
+                                    media_body=media,
+                                    fields='id').execute()
+        pathfile = file.replace('./','')
+        path = rec_folder + "/" + pathfile
+        print('removefile ===> ' + path)
+        os.remove(path)
+
+if __name__ == '__main__':
+    creds = get_credential()
+    rec_radio(CHANNNEL, 16, REC_FOLDER)
+    upload_data(creds, REC_FOLDER, GDRIVE_FOLDER_ID)
---
+++ rec_nhk.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+pid=$$
+date=`date '+%Y-%m-%d-%H_%M'`
+outdir="."
+
+if [ $# -le 1 ]; then
+  echo "usage : $0 channel_name duration(minuites) [outputdir] [prefix]"
+  exit 1
+fi
+
+if [ $# -ge 2 ]; then
+  channel=$1
+  DURATION=`expr $2 \* 60 + 15`
+fi
+if [ $# -ge 3 ]; then
+  outdir=$3
+fi
+PREFIX=${channel}
+if [ $# -ge 4 ]; then
+  PREFIX=$4
+fi
+
+#
+# set channel
+#
+case $channel in
+  "NHK1")
+    aspx="https://nhkradioakr1-i.akamaihd.net/hls/live/511633/1-r1/1-r1-01.m3u8"
+    ;;
+  "NHK2")
+    aspx="https://nhkradioakr2-i.akamaihd.net/hls/live/511929/1-r2/1-r2-01.m3u8"
+    ;;
+  "FM")
+    aspx="https://nhkradioakfm-i.akamaihd.net/hls/live/512290/1-fm/1-fm-01.m3u8"
+    ;;
+  "NHK1_SAPPORO")
+    aspx="https://nhkradioikr1-i.akamaihd.net/hls/live/512098/1-r1/1-r1-01.m3u8"
+    ;;
+  "NHK2_SAPPORO")
+    aspx="https://nhkradioakr2-i.akamaihd.net/hls/live/511929/1-r2/1-r2-01.m3u8"
+    ;;
+  "FM_SAPPORO")
+    aspx="https://nhkradioikfm-i.akamaihd.net/hls/live/512100/1-fm/1-fm-01.m3u8"
+    ;;
+  "NHK1_SENDAI")
+    aspx="https://nhkradiohkr1-i.akamaihd.net/hls/live/512075/1-r1/1-r1-01.m3u8"
+    ;;
+  "NHK2_SENDAI")
+    aspx="https://nhkradioakr2-i.akamaihd.net/hls/live/511929/1-r2/1-r2-01.m3u8"
+    ;;
+  "FM_SENDAI")
+    aspx="https://nhkradiohkfm-i.akamaihd.net/hls/live/512076/1-fm/1-fm-01.m3u8"
+    ;;
+  "NHK1_NAGOYA")
+    aspx="https://nhkradiockr1-i.akamaihd.net/hls/live/512072/1-r1/1-r1-01.m3u8"
+    ;;
+  "NHK2_NAGOYA")
+    aspx="https://nhkradioakr2-i.akamaihd.net/hls/live/511929/1-r2/1-r2-01.m3u8"
+    ;;
+  "FM_NAGOYA")
+    aspx="https://nhkradiockfm-i.akamaihd.net/hls/live/512074/1-fm/1-fm-01.m3u8"
+    ;;
+  "NHK1_OSAKA")
+    aspx="https://nhkradiobkr1-i.akamaihd.net/hls/live/512291/1-r1/1-r1-01.m3u8"
+    ;;
+  "NHK2_OSAKA")
+    aspx="https://nhkradioakr2-i.akamaihd.net/hls/live/511929/1-r2/1-r2-01.m3u8"
+    ;;
+  "FM_OSAKA")
+    aspx="https://nhkradiobkfm-i.akamaihd.net/hls/live/512070/1-fm/1-fm-01.m3u8"
+    ;;
+  "NHK1_HIROSHIMA")
+    aspx="https://nhkradiofkr1-i.akamaihd.net/hls/live/512086/1-r1/1-r1-01.m3u8"
+    ;;
+  "NHK2_HIROSHIMA")
+    aspx="https://nhkradioakr2-i.akamaihd.net/hls/live/511929/1-r2/1-r2-01.m3u8"
+    ;;
+  "FM_IROSHIMA")
+    aspx="https://nhkradiofkfm-i.akamaihd.net/hls/live/512087/1-fm/1-fm-01.m3u8"
+    ;;
+  "NHK1_MATSUYAMA")
+    aspx="https://nhkradiozkr1-i.akamaihd.net/hls/live/512103/1-r1/1-r1-01.m3u8"
+    ;;
+  "NHK2_MATSUYAMA")
+    aspx="https://nhkradioakr2-i.akamaihd.net/hls/live/511929/1-r2/1-r2-01.m3u8"
+    ;;
+  "FM_MATSUYAMA")
+    aspx="https://nhkradiozkfm-i.akamaihd.net/hls/live/512106/1-fm/1-fm-01.m3u8"
+    ;;
+  "NHK1_FUKUOKA")
+    aspx="https://nhkradiolkr1-i.akamaihd.net/hls/live/512088/1-r1/1-r1-01.m3u8"
+    ;;
+  "NHK2_FUKUOKA")
+    aspx="https://nhkradioakr2-i.akamaihd.net/hls/live/511929/1-r2/1-r2-01.m3u8"
+    ;;
+  "FM_FUKUOKA")
+    aspx="https://nhkradiolkfm-i.akamaihd.net/hls/live/512097/1-fm/1-fm-01.m3u8"
+    ;;
+  *)
+    echo "failed channel"
+    exit 1
+    ;;
+esac
+
+ffmpeg -loglevel quiet -y -t ${DURATION} -i ${aspx} -acodec libmp3lame -ab 128k "${outdir}/${PREFIX}_${date}.mp3"
+#avconv -loglevel quiet -y -t ${DURATION} -i ${aspx} -acodec mp3 -ab 128k "${outdir}/${PREFIX}_${date}.mp3"
